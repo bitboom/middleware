219dbcd2d36bbbdcbf17ff90208083fa91d174ea
seolheui,kim <s414.kim@samsung.com>
2016-08-08 19:09:08 +0900

    Add password confirm mode to dpm-syspopup
    Change-Id: I0a2deb6b52bae342cd11434b08ed5bd178a9b48e
    Signed-off-by: seolheui,kim <s414.kim@samsung.com>

--- tools/syspopup/src/ui.c
+++ tools/syspopup/src/ui.c
@@ -19,32 +19,32 @@
 
 #include <notification.h>
 #include <efl_util.h>
+#include <vconf.h>
 
 #include "dpm-syspopup.h"
+
+static Evas_Object *main_window;
 
 static void __win_delete_request_cb(void *data, Evas_Object *obj, void *event_info)
 {
 	ui_app_exit();
 }
 
-static Evas_Object *__create_win(const char *pkg_name)
+static void create_window(const char *pkg_name)
 {
 	int rots[] = {0, 90, 180, 270};
+	main_window = elm_win_add(NULL, pkg_name, ELM_WIN_NOTIFICATION);
+	efl_util_set_notification_window_level(main_window, EFL_UTIL_NOTIFICATION_LEVEL_DEFAULT);
 
-	Evas_Object *win = elm_win_add(NULL, pkg_name, ELM_WIN_NOTIFICATION);
-	efl_util_set_notification_window_level(win, EFL_UTIL_NOTIFICATION_LEVEL_DEFAULT);
+	if (elm_win_wm_rotation_supported_get(main_window))
+		elm_win_wm_rotation_available_rotations_set(main_window, rots, 4);
 
-	if (elm_win_wm_rotation_supported_get(win))
-		elm_win_wm_rotation_available_rotations_set(win, rots, 4);
+	elm_win_title_set(main_window, pkg_name);
+	elm_win_borderless_set(main_window, EINA_TRUE);
+	elm_win_alpha_set(main_window, EINA_TRUE);
 
-	elm_win_title_set(win, pkg_name);
-	elm_win_borderless_set(win, EINA_TRUE);
-	elm_win_alpha_set(win, EINA_TRUE);
-
-	evas_object_smart_callback_add(win, "delete,request", __win_delete_request_cb, NULL);
-	evas_object_show(win);
-
-	return win;
+	evas_object_smart_callback_add(main_window, "delete,request", __win_delete_request_cb, NULL);
+	evas_object_show(main_window);
 }
 
 static void __popup_del_cb(void *data, Evas *e, Evas_Object *obj, void *event_info)
@@ -64,7 +64,7 @@ static void __block_clicked_cb(void *data, Evas_Object *obj, void *event_info)
 	return;
 }
 
-static void __ok_btn_cb(void *data, Evas_Object *obj, void *event_info)
+static void default_ok_button_cb(void *data, Evas_Object *obj, void *event_info)
 {
 	Evas_Object *popup = (Evas_Object *) data;
 	evas_object_data_set(popup, "selected", "ok");
@@ -78,6 +78,73 @@ static void __ok_btn_cb(void *data, Evas_Object *obj, void *event_info)
 	app_control_destroy(app_control);
 	evas_object_del(popup);
 	return;
+}
+
+static void reply_password_enforce_event_handler(app_control_h ug, app_control_h reply, app_control_result_e result, void* data)
+{
+	Evas_Object *popup = (Evas_Object *) data;
+	if (!result) {
+		char *result_string = NULL;
+		char *current = NULL;
+
+		app_control_get_extra_data(reply, "result", &result_string);
+		if (result_string == NULL) {
+			dlog_print(DLOG_ERROR, LOG_TAG, "failed to get reply.");
+			evas_object_del(popup);
+			return;
+		}
+
+		free(result_string);
+
+		if (strcmp(result_string, "SETTING_PW_TYPE_ENTER_LOCK_TYPE") ||
+				 strcmp(result_string, "SETTING_PW_TYPE_VERIFY_FP_ALT_PASSWORD")) {
+
+			app_control_get_extra_data(reply, "current", &current);
+			if (current != NULL) {
+				app_control_h app_control;
+				if (app_control_create(&app_control) != APP_CONTROL_ERROR_NONE) {
+					dlog_print(DLOG_ERROR, LOG_TAG, "faild to create app_control context");
+					evas_object_del(popup);
+					return;
+				}
+
+				app_control_add_extra_data(app_control, "current", current);
+				app_control_add_extra_data(app_control, "caller", "DPM");
+				app_control_set_app_id(app_control, "setting-locktype-efl");
+				app_control_send_launch_request(app_control, NULL, NULL);
+				app_control_destroy(app_control);
+				free(current);
+			} else {
+				dlog_print(DLOG_ERROR, LOG_TAG, "failed to get current password.");
+				evas_object_del(popup);
+				return;
+			}
+		}
+	}
+	evas_object_del(popup);
+}
+
+static void password_enforce_event_handler(void *data, Evas_Object *obj, void *event_info)
+{
+	int lock_type = 0;
+	Evas_Object *popup = (Evas_Object *) data;
+
+	elm_win_lower(main_window);
+
+	evas_object_data_set(popup, "selected", "ok");
+	app_control_h app_control = (app_control_h)evas_object_data_get(popup, "app-control");
+
+	vconf_get_int(VCONFKEY_SETAPPL_SCREEN_LOCK_TYPE_INT, &lock_type);
+
+	if (lock_type == SETTING_SCREEN_LOCK_TYPE_PASSWORD || lock_type == SETTING_SCREEN_LOCK_TYPE_SIMPLE_PASSWORD) {
+		app_control_add_extra_data(app_control, "viewtype", "SETTING_PW_TYPE_ENTER_LOCK_TYPE");
+		app_control_send_launch_request(app_control, reply_password_enforce_event_handler, popup);
+	} else {
+		app_control_set_app_id(app_control, "setting-locktype-efl");
+		app_control_send_launch_request(app_control, NULL, NULL);
+		evas_object_del(popup);
+	}
+	app_control_destroy(app_control);
 }
 
 static int __set_notification(notification_h noti_handle, app_control_h app_control, char *title, char *content)
@@ -107,7 +174,7 @@ static int __set_notification(notification_h noti_handle, app_control_h app_cont
 	return ret;
 }
 
-static void __cancel_btn_cb(void *data, Evas_Object *obj, void *event_info)
+static void cancel_button_cb(void *data, Evas_Object *obj, void *event_info)
 {
 	Evas_Object *popup = (Evas_Object *) data;
 	evas_object_data_set(popup, "selected", "cancel");
@@ -172,13 +239,17 @@ static void __create_popup_btn(Evas_Object *popup, char *part, char *btn_text, E
 
 void _create_syspopup(const char *id, char *style, const char *status, app_control_h svc)
 {
-	Evas_Object *win = NULL;
 	Evas_Object *popup = NULL;
-
+	Evas_Smart_Cb ok_button_handler = NULL;
 	popup_info_s *info = NULL;
 	int ret = 0;
 	char header[PATH_MAX] = "\0";
 	char body[PATH_MAX] = "\0";
+
+	if (!strcmp(id, "password-enforce-change"))
+		ok_button_handler = password_enforce_event_handler;
+	else
+		ok_button_handler = default_ok_button_cb;
 
 	info = _get_popup_info(id);
 	if (info == NULL)
@@ -190,15 +261,15 @@ void _create_syspopup(const char *id, char *style, const char *status, app_contr
 		return;
 	}
 
-	win = __create_win("dpm-syspopup");
-	popup = elm_popup_add(win);
+	create_window("dpm-syspopup");
+	popup = elm_popup_add(main_window);
 	evas_object_size_hint_weight_set(popup, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
 
 	if (style != NULL)
 		info->style = style;
 
 	elm_object_style_set(popup, info->style);
-	eext_object_event_callback_add(popup, EEXT_CALLBACK_BACK, eext_popup_back_cb, win);
+	eext_object_event_callback_add(popup, EEXT_CALLBACK_BACK, eext_popup_back_cb, main_window);
 
 	if (!strcmp(info->style, "default")) {
 		if (header != NULL) {
@@ -216,10 +287,10 @@ void _create_syspopup(const char *id, char *style, const char *status, app_contr
 		if (info->left_btn != NULL) {
 			ecore_event_handler_add(ECORE_EVENT_KEY_DOWN, __home_key_cb, popup);
 
-			__create_popup_btn(popup, "button1", info->left_btn, __cancel_btn_cb);
-			__create_popup_btn(popup, "button2", info->right_btn, __ok_btn_cb);
+			__create_popup_btn(popup, "button1", info->left_btn, cancel_button_cb);
+			__create_popup_btn(popup, "button2", info->right_btn, ok_button_handler);
 		} else {
-			__create_popup_btn(popup, "button1", info->right_btn, __ok_btn_cb);
+			__create_popup_btn(popup, "button1", info->right_btn, ok_button_handler);
 
 			eext_object_event_callback_del(popup, EEXT_CALLBACK_BACK, eext_popup_back_cb);
 			/*[TBD] eext_win_keygrab_set(win, "XF86Home");*/
