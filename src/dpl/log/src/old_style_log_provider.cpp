1f82da6245aad8026135b28e0ee24ac88b48b108
sangwan.kwon <sangwan.kwon@samsung.com>
2016-12-22 13:41:53 +0900

    Cast LogLevel to int explicitly before use as int
    Change-Id: I8faaaf896cc4739217f5c8d8f201043ef3c06ef1
    Signed-off-by: sangwan.kwon <sangwan.kwon@samsung.com>

--- src/dpl/log/src/old_style_log_provider.cpp
+++ src/dpl/log/src/old_style_log_provider.cpp
@@ -95,7 +95,7 @@ void OldStyleLogProvider::Log(AbstractLogProvider::LogLevel level,
 			std::string("] ") << function << std::string("(): ") << message << mark.end;
 		fprintf(stdout, "%s\n", val.str().c_str());
 	} catch (const std::out_of_range &) {
-		fprintf(stdout, "Unsupported log level: %d\n", level);
+		fprintf(stdout, "Unsupported log level: %d\n", static_cast<int>(level));
 	}
 }
 
