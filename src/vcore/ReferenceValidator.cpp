154f2d38cff7f86fc0e50c5784f937a76b699ca8
Tomasz Swierczek <t.swierczek@samsung.com>
2020-01-10 07:06:52 +0100

    Adjust to gcc 9
    Change-Id: I74da408463618ec011f83be49573689eda5036eb

--- src/vcore/ReferenceValidator.cpp
+++ src/vcore/ReferenceValidator.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2019 Samsung Electronics Co., Ltd. All rights reserved
+ * Copyright (c) 2016-2020 Samsung Electronics Co., Ltd. All rights reserved
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -25,6 +25,7 @@
 #include <dirent.h>
 #include <errno.h>
 #include <fstream>
+#include <functional>
 #include <memory>
 #include <limits.h>
 #include <sys/types.h>
--- src/vcore/SignatureFinder.cpp
+++ src/vcore/SignatureFinder.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+ * Copyright (c) 2016-2020 Samsung Electronics Co., Ltd All Rights Reserved
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -24,6 +24,7 @@
 
 #include <dirent.h>
 #include <errno.h>
+#include <functional>
 #include <istream>
 #include <sstream>
 #include <memory>
